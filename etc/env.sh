ee5053cb58c7364809ca4b3ba951260038f913d3
John Bellone <jb@thunkbrightly.com>
2012-08-09 22:48:50 -0400

    Adding some changes here for colors.

--- etc/env.sh
+++ etc/env.sh
@@ -1,3 +1,4 @@
 export CLICOLORS=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/Users/jbellone/.rvm/gems/ruby-1.9.3-p194/bin:/usr/local/pear/bin
 
