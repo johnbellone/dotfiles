0699f694575d650719b48c07c05d88c9d5e32ff0
John Bellone <jb@thunkbrightly.com>
2012-08-22 22:50:11 -0400

    Change the zsh prompt to something more simple.
    I got tired of git hogging all the cpu cycles. This should be
    done somewhere else. I spend most of my time in emacs and that
    shows me the latest commit/branch. Why the hell do I need it in
    my prompt?

--- etc/zsh/zshrc.zsh
+++ etc/zsh/zshrc.zsh
@@ -2,7 +2,8 @@ DOT=$HOME/dotfiles
 ZSH=$HOME/.zsh
 
 source $DOT/etc/env.sh
-source $ZSH/git-prompt/zshrc.sh
+#source $ZSH/git-prompt/zshrc.sh
 
 autoload -U promptinit && promptinit
-PROMPT='%F{24}%* %E%F{228}${(%):-%~} %F{231}%#%E %F{254}'
+PROMPT='%F{24}%* %E%F{228} %F{231}%#%E %F{254}'
+
