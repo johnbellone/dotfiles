07e1b9fb449acb72021be179047c4322bef6a733
John Bellone <jb@thunkbrightly.com>
2012-09-14 19:38:28 -0400

    Adding hostname to the prompt, removing env.sh file.

--- etc/zsh/zshrc.zsh
+++ etc/zsh/zshrc.zsh
@@ -1,9 +1,6 @@
 DOT=$HOME/dotfiles
 ZSH=$HOME/.zsh
 
-source $DOT/etc/env.sh
-#source $ZSH/git-prompt/zshrc.sh
-
 autoload -U promptinit && promptinit
-PROMPT='%F{24}%* %E%F{228} %F{231}%#%E %F{254}'
+PROMPT='%F{24}%* %E%F{228} %m%F{231}%#%E %F{254}'
 
