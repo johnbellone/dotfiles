cd693ee018b5ba84e5447defb0331bf686e3a424
John Bellone <jb@thunkbrightly.com>
2013-05-07 20:20:36 -0400

    Adds some more aliases and comments.
    Because I don't want to have to look into the git history each and every
    time I try to figure out why I added an alias.

--- etc/bash/bash_aliases.bash
+++ etc/bash/bash_aliases.bash
@@ -1,14 +1,28 @@
 # -*- mode: sh -*-
 # ~/.bash_aliases
 
-alias ls='ls --color=auto'
+# NOTE: This is mainly due to the fact that BSD (Mac OS X) ships with
+# a different toolchain. At some point it'd be nice to actually have
+# the GNU tools for everything.
+case $OSTYPE in
+    Linux)
+        alias ls='ls --color=auto'
+        ;;
+    *)
+        ;;
+esac
+
+# Directory listing made easy.
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+# Make sure that my grep is colorized.
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+# Some additional utility aliases.
+alias stalk="ps auxx | grep -v 'grep --color=auto -i' | grep -i"
 alias df='df | column -t'
 alias du='du -H'
