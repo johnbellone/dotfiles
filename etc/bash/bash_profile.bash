466dc7bdfc15d9340b0b3620fb67c5b58fe84c73
John Bellone <john.bellone.jr@gmail.com>
2013-05-25 23:35:45 -0400

    Add node and npm version to bash_profile.

--- etc/bash/bash_profile.bash
+++ etc/bash/bash_profile.bash
@@ -4,11 +4,14 @@
 # Source all of additional files if they're sitting around.
 [ -f $HOME/.javarc ] && source $HOME/.javarc
 [ -f $HOME/.rubyrc ] && source $HOME/.rubyrc
+[ -f $HOME/.jsrc ] && source $HOME/.jsrc
 [ -f $HOME/.bashrc ] && source $HOME/.bashrc
 
 # Print out some diagnostics on a login shell.
 [ -f $RUBY_PATH ] && echo "$(ruby --version)"
 [ -f $JAVA_PATH ] && echo "$(java -version 2>&1 | head -n2 | tail -n1)"
+[ -f $NODE_PATH ] && echo "node $(node --version)"
+[ -f $NPM_PATH ] && echo "npm $(npm --version)"
 
 # Finally, print out our uptime of this machine.
 echo "$(uptime)"
