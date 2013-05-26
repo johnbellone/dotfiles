8d76c1cd43f88a5610a1c224b2c672d07dcc91ac
John Bellone <john.bellone.jr@gmail.com>
2013-05-26 10:25:40 -0400

    Refactor when diagnostics get print.
    Print out all of our information at the top regarding the machine
    itself. This is because some /etc/profile entries may do funky things on
    different machines.
    
    I would rather have all the versions be last anyway.

--- etc/bash/bash_profile.bash
+++ etc/bash/bash_profile.bash
@@ -1,6 +1,9 @@
 # -*- mode: sh -*-
 # ~/.bash_profile
 
+# Print out useful information about this machine.
+echo "uptime $(uptime)"
+
 # Source all of additional files if they're sitting around.
 [ -f $HOME/.bashrc ] && source $HOME/.bashrc
 [ -f $HOME/.javarc ] && source $HOME/.javarc
@@ -11,6 +14,3 @@
 [ -f $RUBY_PATH ] && echo "$(ruby --version)"
 [ -f $JAVA_PATH ] && echo "$(java -version 2>&1 | head -n2 | tail -n1)"
 [ -f $NODE_PATH ] && [ -f $NPM_PATH ] && echo "nodejs $(node --version) npm $(npm --version)"
-
-# Finally, print out our uptime of this machine.
-echo "$(uptime)"
