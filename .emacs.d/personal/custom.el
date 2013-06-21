8239cdd5be5cb622f45c9fb4ab6784e8512e5aa0
John Bellone <john.bellone.jr@gmail.com>
2013-06-21 07:06:22 -0400

    Move emacs configuration to .emacs.d/personal.
    This is for using the prelude scripts.

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -1,4 +1,5 @@
 ;;; package --- Custom
+;;; Code
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (prelude-ensure-module-deps '(robe ruby-block creole flymake-coffee flymake-css flymake-json coffee-mode csv-mode coffee-fof powerline))
