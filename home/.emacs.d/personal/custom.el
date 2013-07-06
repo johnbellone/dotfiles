ab8904ccae9a899bf2c068cd1d7d4c73863e2f1b
John Bellone <john.bellone.jr@gmail.com>
2013-07-06 14:25:02 -0400

    Remove block mode.

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -6,27 +6,25 @@
 ;; Setup syntax highlighting for files.
 (add-to-list 'auto-mode-alist '("\\Capfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Cheffile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Berksfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Godfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gearfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Procfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Vagrantfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Rakefile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\*.rb" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\*.god" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\*.rake" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\*.gemspec" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\*.gear" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.god$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gear$" . enh-ruby-mode))
 
 (prelude-ensure-module-deps
  '(powerline rbenv robe puppet-mode
    ruby-hash-syntax ruby-interpolation ruby-test-mode
    ruby-tools rspec-mode rubocop wgrep yaml-mode yard-mode
    apache-mode coffee-mode coffee-fof crontab-mode csv-mode
-   csharp-mode dired+ enh-ruby-mode js2-mode
+   csharp-mode dired+ enh-ruby-mode js3-mode
    fic-mode flyspell-lazy google-c-style haml-mode jump
    magit-gh-pulls magit-push-remote nginx-mode flymake-ruby
-   flymake-json flymake-yaml ruby-end ruby-block))
+   flymake-json flymake-yaml ruby-end))
 
 (setq enh-ruby-program "/Users/jbellone/.rbenv/shims/ruby")
 (setq enh-ruby-bounce-deep-indent 1)
