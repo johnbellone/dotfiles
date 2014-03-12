fa9452ae1e82d902b9430af4779f9a4184686e80
John Bellone <jbellone@bloomberg.net>
2014-03-12 13:39:33 -0400

    Setup autopair mode for ruby.

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -76,10 +76,7 @@
 (use-package autopair
   :diminish t
   :config
-  (progn
-    (add-hook 'enh-ruby-mode
-              '(lambda ()
-                 (autopair-mode 1)))))
+  (progn (add-hook 'enh-ruby-mode-hook 'autopair-mode)))
 
 (use-package enh-ruby-mode
   :ensure autopair
@@ -96,6 +93,7 @@
     (add-to-list 'auto-mode-alist '("Gearfile\\'" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("Vagrantfile\\'" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("Gemfile\\'" . enh-ruby-mode))
+    (add-to-list 'auto-mode-alist '("Thorfile\\'" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("Rakefile\\'" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("\\.god$" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
@@ -174,3 +172,9 @@
 
 (provide 'custom)
 ;;; custom.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Inconsolata")))))
