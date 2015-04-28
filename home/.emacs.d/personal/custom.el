3ed703b3d618a0dd965d601e8fbe0d9a36c0ff14
John Bellone <jbellone@bloomberg.net>
2015-04-28 09:28:47 -0400

    Spruce up my elisp a bit.

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -1,49 +1,20 @@
 ;;; custom --- Customized settings for my Emacs environment.
 ;;
-;; Copyright © 2011-2014 John Bellone <john.bellone.jr@gmail.com>
+;; Copyright © 2011-2015 John Bellone <jbellone@bellone.us>
 ;;
-;;; License:
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-;;
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-;;
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-;; THE SOFTWARE.
-
 ;;; Commentary:
-;; For the longest time I actually maintained my own Emacs settings.  This was
-;; was an absolute, pain the ass.  Once I found Prelude I decided that I was no
-;; longer going to waste my time doing this.  Rather than that I decided that
-;; I would spend my time, like many others, customizing the foundation that has
-;; been so graciously provided.
-
+;; Write something meaningful here.
 ;;; Code:
+
+(setq whitespace-line-column 120)
 
 (prelude-ensure-module-deps '(use-package))
 (require 'use-package)
 
-(require 'chruby)
+(use-package ox-reveal
+  :config (progn
+            (require 'ox-reveal)))
 
-(setq whitespace-line-column 120)
-
-(use-package chruby
-  :config (progn (chruby "2.1.2")))
-(use-package company
-  :config
-  (progn
-    (use-package company-inf-ruby)
-    (add-hook 'after-init-hook 'global-company-mode)))
 (use-package diminish)
 (use-package flyspell-lazy)
 (use-package yaml-mode
@@ -77,57 +48,63 @@
   (progn
     (use-package ack-menu)))
 
+(use-package chruby
+  :config (progn
+            (require 'chruby)
+            (chruby "2.2")))
+
+(use-package company
+  :config (progn
+            (use-package company-inf-ruby)
+            (add-hook 'after-init-hook 'global-company-mode)))
+
 (use-package enh-ruby-mode
   :ensure autopair
   :mode ("\\.rb$" . enh-ruby-mode)
   :interpreter ("ruby" . enh-ruby-mode)
-  :init
-  (progn
-    ;; Add files to the global font-lock list for this mode.
-    (add-to-list 'auto-mode-alist '("Capfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Cheffile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Guardfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Berksfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Godfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Gearfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Vagrantfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Gemfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Thorfile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("Rakefile\\'" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.god$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.task$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.cap$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
-    (add-to-list 'auto-mode-alist '("\\.gear$" . enh-ruby-mode)))
-  :config
-  (progn
-    (set 'enh-ruby-bounce-deep-indent 1)
-
-    (use-package rubocop
-      :config
-      (progn (add-hook 'enh-ruby-mode-hook 'rubocop-mode)))
-    (use-package company-inf-ruby
-      :diminish t
-      :ensure company)
-    (use-package yard-mode
-      :diminish t
-      :config
-      (progn (add-hook 'enh-ruby-mode-hook 'yard-mode)))
-    (use-package ruby-block
-      :diminish t
-      :config
-      (progn (add-hook 'enh-ruby-mode-hook 'ruby-block-mode)))
-    (use-package inf-ruby
-      :config
-      (progn
-        (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-        (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)))
-    (use-package robe
-      :diminish t
-      :config
-      (progn (add-hook 'enh-ruby-mode-hook 'robe-mode)))))
+  :init (progn
+          ;; Add files to the global font-lock list for this mode.
+          (add-to-list 'auto-mode-alist '("Capfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Cheffile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Guardfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Berksfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Godfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Gearfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Vagrantfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Gemfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Thorfile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("Rakefile\\'" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.god$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.task$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.cap$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+          (add-to-list 'auto-mode-alist '("\\.gear$" . enh-ruby-mode)))
+  :config (progn
+            (set 'enh-ruby-bounce-deep-indent 1)
+            (use-package rubocop
+              :config (progn
+                        (add-hook 'enh-ruby-mode-hook 'rubocop-mode)))
+            (use-package company-inf-ruby
+              :diminish t
+              :ensure company)
+            (use-package yard-mode
+              :diminish t
+              :config (progn
+                        (add-hook 'enh-ruby-mode-hook 'yard-mode)))
+            (use-package ruby-block
+              :diminish t
+              :config (progn
+                        (add-hook 'enh-ruby-mode-hook 'ruby-block-mode)))
+            (use-package inf-ruby
+              :config (progn
+                        (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+                        (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)))
+            (use-package robe
+              :diminish t
+              :config (progn
+                        (add-hook 'enh-ruby-mode-hook 'robe-mode)))))
 
 (use-package go-mode
   :defer t
