97b162d4b2c1ca263b7dce3099864ca5aaeb552a
John Bellone <jbellone@bellone.us>
2016-01-18 10:50:57 -0500

    Install certain packages only on OSX.

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -16,10 +16,12 @@
 (use-package google-c-style :ensure t)
 (use-package ssh-config-mode :ensure t)
 
-(use-package osx-browse :ensure t)
-(use-package osx-clipboard :ensure t)
-(use-package osx-lib :ensure t)
-(use-package osx-pseudo-daemon :ensure t)
+(if (eq system-type "darwin")
+    (progn
+      (use-package osx-browse :ensure t)
+      (use-package osx-clipboard :ensure t)
+      (use-package osx-lib :ensure t)
+      (use-package osx-pseudo-daemon :ensure t)))
 
 (use-package org
   :ensure t
@@ -167,7 +169,59 @@
   (DELETE 2)
   (HEAD 2)
   (ANY 2)
-  (context 2))
+  (context 2)
+  (defroutes* 'defun)
+  (GET* 2)
+  (POST* 2)
+  (PUT* 2)
+  (DELETE* 2)
+  (HEAD* 2)
+  (ANY* 2)
+  (context* 2))
 
 (provide 'custom)
 ;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#28211c" "#cf6a4c" "#54be0d" "#f9ee98" "#5ea6ea" "#9b859d" "#5ea6ea" "#8a8986"])
+ '(ansi-term-color-vector
+   [unspecified "#28211c" "#cf6a4c" "#54be0d" "#f9ee98" "#5ea6ea" "#9b859d" "#5ea6ea" "#8a8986"])
+ '(custom-safe-themes
+   (quote
+    ("b110da1a5934e91717b5c490709aba3c60eb4595194bbf9fdcbb97d247c70cfa" "db9feb330fd7cb170b01b8c3c6ecdc5179fc321f1a4824da6c53609b033b2810" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
