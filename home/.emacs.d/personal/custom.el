b36c2a5f772098f5ad80d85f1d2570262eebb4a4
John Bellone <jbellone@bloomberg.net>
2018-03-12 13:59:49 -0400

    emacs: Update org-mode configuration and remove cider-modes.

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -4,17 +4,17 @@
 ;;; Code:
 
 (require 'package)
-
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-(setq user-full-name "John Bellone"
-      user-mail-address "john.bellone.jr@gmail.com")
-
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+(setq user-full-name "John Bellone"
+      user-mail-address "john.bellone.jr@gmail.com")
 
 (setq load-prefer-newer t
       gc-cons-threshold 50000000
@@ -41,20 +41,50 @@
 (define-key emacs-lisp-mode-map (kbd "<S-tab>")  'lisp-complete-symbol)
 
 (use-package org
-  :ensure t
+  :init
+  (add-hook 'org-mode-hook 'visual-line-mode)
+  (add-hook 'org-mode-hook 'org-indent-mode)
+  (add-hook 'org-mode-hook 'flyspell-mode)
+  :diminish visual-line-mode
+  :diminish org-indent-mode
+  :defer t
+  :bind (("\C-c a" . org-agenda)
+         ("\C-c c" . org-capture))
   :config
-  (progn
-    (setq org-startup-indented t
-          org-startup-folded "showall"
-          org-directory "~/org"
-          org-src-fontify-natively t
-          org-use-speed-commands t)))
-
+  (setq org-directory "~/org"
+        org-default-notes-file "~/org/refile.org"
+        org-agenda-files (quote "~/org"))
+  (defvar org-default-diary-file "~/org/diary.org")
+  (setq org-startup-indented t
+        org-startup-folded "showall"
+        org-cycle-separator-lines 0
+        org-tags-column 80
+        org-agenda-tags-column org-tags-column
+        org-agenda-sticky t
+        org-src-fontify-natively t
+        org-use-speed-commands t)
+  (setq org-columns-default-format "%50ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM %16TIMESTAMP_IA")
+  (setq org-todo-keyword-faces
+        '(("TODO" :foreground "red" :weight bold)
+          ("NEXT" :foreground "blue" :weight bold)
+          ("DONE" :foreground "forest green" :weight bold)
+          ("WAITING" :foreground "orange" :weight bold)
+          ("INACTIVE" :foreground "magenta" :weight bold)
+          ("CANCELLED" :foreground "forest green" :weight bold)
+          ("MEETING" :foreground "forest green" :weight bold)))
+  (setq org-todo-state-tags-triggers
+        '(("CANCELLED" ("CANCELLED" . t))
+          ("WAITING" ("WAITING" . t))
+          ("INACTIVE" ("WAITING") ("INACTIVE" . t))
+          (done ("WAITING") ("INACTIVE"))
+          ("TODO" ("WAITING") ("CANCELLED") ("INACTIVE"))
+          ("NEXT" ("WAITING") ("CANCELLED") ("INACTIVE"))
+          ("DONE" ("WAITING") ("CANCELLED") ("INACTIVE")))))
+                                      
 (use-package gitattributes-mode :ensure t)
 (use-package gitconfig-mode :ensure t)
 (use-package gitignore-mode :ensure t)
 (use-package google-c-style
-  :ensure t
   :config
   (progn
     (add-hook 'c-mode-common-hook 'google-set-c-style)
@@ -62,6 +92,8 @@
 (use-package ssh-config-mode :ensure t)
 
 (when (eq system-type "darwin")
+  (set-face-attribute 'default nil :family "Inconsolata-dz")
+  (set-face-attribute 'default nil :height 140)
   (setq mac-option-modifier 'alt)
   (setq mac-command-modifier 'meta)
   (global-set-key [kp-delete] 'delete-char)
@@ -71,89 +103,34 @@
   (use-package osx-lib :ensure t)
   (use-package osx-pseudo-daemon :ensure t))
 
-(use-package org
-  :ensure t
-  :config
-  (progn
-    (use-package org-ac)
-    (use-package org-autolist)
-    (use-package org-journal)
-    (use-package org-projectile)
-    (use-package orgit)
-    (use-package ox-pandoc)
-    (use-package ox-reveal)
+(use-package company)
+(use-package company-inf-ruby
+  :after company
+  :config (add-hook 'after-init-hook 'global-company-mode))
 
-    (use-package org2jekyll
-      :config
-      (progn
-        (setq org2jekyll-blog-author "jbellone")
-        (setq org2jekyll-jekyll-posts-dir "_posts/")
-        (setq org2jekyll-source-directory (expand-file-name "~/Projects/thoughtlessbanter.com"))))))
+(use-package json-mode)
+(use-package flymake-json :after json-mode)
 
-(use-package chruby
-  :ensure t
-  :config
-  (chruby "2.3"))
+(use-package helm)
+(use-package ac-helm
+  :after helm
+  :config (add-to-list 'ac-modes 'helm-mode))
 
-(use-package company
-  :ensure t
-  :config
-  (progn
-    (use-package company-inf-ruby)
-    (add-hook 'after-init-hook 'global-company-mode)))
+(use-package coffee-mode)
+(use-package flymake-coffee :after coffee-mode)
 
-(use-package json-mode
-  :ensure t
-  :config
-  (use-package flymake-json :ensure t))
+(use-package yaml-mode)
+(use-package flymake-yaml :after yaml-mode)
 
-(use-package cider
-  :ensure t
-  :config
-  (progn
-    (use-package ac-cider)
-    (use-package cider-decompile)
-    (use-package cider-eval-sexp-fu)
-    (use-package cider-profile)
-    (use-package cider-spy)
-    (use-package clojure-mode-extra-font-locking)
-    (use-package clj-refactor)
-    (use-package cljr-helm)
-    (use-package flycheck-clojure)
-    (use-package inf-clojure)))
+(use-package go-mode)
+(use-package go-autocomplete
+  :after go-mode
+  :config (add-to-list 'ac-modes 'go-mode))
+(use-package go-eldoc :after go-mode)
 
-(use-package helm
-  :ensure t
-  :config
-  (progn
-    (use-package ac-helm
-      :ensure t
-      :config
-      (add-to-list 'ac-modes 'helm-mode))))
+(use-package salt-mode)
 
-(use-package coffee-mode
-  :ensure t
-  :config
-  (use-package flymake-coffee :ensure t))
-
-(use-package yaml-mode
-  :ensure t
-  :config
-  (use-package flymake-yaml :ensure t))
-
-(use-package go-mode
-  :ensure t
-  :config
-  (progn
-    (use-package go-autocomplete
-      :ensure t
-      :config
-      (add-to-list 'ac-modes 'go-mode))
-    (use-package go-eldoc :ensure t)))
-
-(use-package salt-mode :ensure t)
 (use-package jinja2-mode
-  :ensure t
   :mode ("\\.jinja$" . jinja2-mode))
 
 (use-package enh-ruby-mode
@@ -181,27 +158,21 @@
     (add-to-list 'auto-mode-alist '("Guardfile$" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("Thorfile$" . enh-ruby-mode))
     (add-to-list 'auto-mode-alist '("Vagrantfile$" . enh-ruby-mode))))
-
 (use-package ruby-electric
-  :ensure t
-  :config
-  (add-hook 'enh-ruby-mode-hook 'ruby-electric-mode))
+  :after enh-ruby-mode
+  :config (add-hook 'enh-ruby-mode-hook 'ruby-electric-mode))
 (use-package yard-mode
-  :ensure t
-  :config
-  (add-hook 'enh-ruby-mode-hook 'yard-mode))
+  :after enh-ruby-mode
+  :config (add-hook 'enh-ruby-mode-hook 'yard-mode))
 (use-package robe
-  :ensure t
-  :config
-  (add-hook 'enh-ruby-mode-hook 'robe-mode))
+  :after enh-ruby-mode
+  :config (add-hook 'enh-ruby-mode-hook 'robe-mode))
 (use-package ruby-block
-  :ensure t
-  :config
-  (add-hook 'enh-ruby-mode-hook 'ruby-block-mode))
+  :after enh-ruby-mode
+  :config (add-hook 'enh-ruby-mode-hook 'ruby-block-mode))
 (use-package inf-ruby
-  :ensure t
-  :config
-  (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode))
+  :after enh-ruby-mode
+  :config (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode))
 
 ;; http://www.emacswiki.org/emacs/EmacsClient#WMFocus
 (add-hook 'server-switch-hook
@@ -211,25 +182,6 @@
             (when server-buffer-clients
               (local-set-key (kbd "C-x k") 'server-edit))))
 
-(define-clojure-indent
-  (defroutes 'defun)
-  (GET 2)
-  (POST 2)
-  (PUT 2)
-  (DELETE 2)
-  (HEAD 2)
-  (ANY 2)
-  (context 2)
-  (defroutes* 'defun)
-  (GET* 2)
-  (POST* 2)
-  (PUT* 2)
-  (DELETE* 2)
-  (HEAD* 2)
-  (ANY* 2)
-  (context* 2))
-
-(provide 'custom)
 ;;; custom.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -238,7 +190,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (salt-mode jinja2-mode enh-ruby-mode markdown-mode ssh-config-mode google-c-style gitattributes-mode use-package yaml-mode web-mode scss-mode geiser ensime scala-mode yari inf-ruby ruby-tools company-anaconda anaconda-mode flycheck-ocaml merlin utop tuareg company-auctex cdlatex auctex json-mode js2-mode haskell-mode gotest go-projectile go-eldoc company-go go-mode alchemist elixir-mode erlang rainbow-mode elisp-slime-nav slime coffee-mode cider clojure-mode rainbow-delimiters key-chord company helm-ag helm-descbinds helm-projectile helm smex ido-completing-read+ flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+    (orca org-agenda-property org-doing org-ref osx-clipboard osx-lib osx-plist osx-trash org-projectile-helm docker docker-api docker-compose-mode docker-tramp dockerfile-mode groovy-mode flycheck-plantuml plantuml-mode protobuf-mode markdown-mode ac-c-headers ac-cider ac-etags ac-html ac-inf-ruby ace-flyspell cider-decompile cider-eval-sexp-fu cider-hydra cider-profile cider-spy clj-refactor cljr-helm clojure-mode-extra-font-locking flycheck-clojure inf-clojure org-ac org-autolist org-journal org-projectile company-inf-ruby zop-to-char zenburn-theme yari yard-mode which-key web-mode volatile-highlights vkill use-package undo-tree ssh-config-mode smex smartrep smartparens smart-mode-line slime salt-mode ruby-tools ruby-electric ruby-block robe rainbow-mode rainbow-delimiters racer ov operate-on-number move-text magit key-chord json-mode js2-mode jinja2-mode imenu-anywhere ido-completing-read+ helm-projectile helm-descbinds helm-ag guru-mode grizzl gotest google-c-style god-mode go-projectile go-autocomplete gitignore-mode gitconfig-mode gitattributes-mode git-timemachine gist geiser flymake-yaml flymake-json flymake-coffee flycheck-rust flx-ido expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-go company-auctex company-anaconda coffee-mode cider chruby cdlatex cargo browse-kill-ring beacon anzu ace-window ac-helm)))
+ '(plantuml-jar-path "/usr/local/Cellar/plantuml/1.2018.1/libexec/plantuml.jar"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
